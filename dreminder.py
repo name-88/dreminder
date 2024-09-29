7d72676ee98df5589789111f67091d080ee81aae
name-88 <sasha33433@gmail.com>
2024-09-29 11:54:55 +0300

    добавлена функция set

--- dreminder.py
+++ dreminder.py
@@ -5,6 +5,24 @@ import datetime
 import time
 import pygame
 
+t = None
+
+def set():
+    global t
+    rem = sd.askstring("Время напоминания", "Введите время в формате ЧЧ:ММ (24-часовой формат)")
+    if rem:
+        try:
+            hour = int(rem.split(":")[0])
+            minute = int(rem.split(":")[1])
+            now = datetime.datetime.now()
+            print(now)
+            dt = now.replace(hour=hour, minute=minute)
+            print(dt)
+            t = dt.timestamp()
+            print(t)
+            label.config(text=f"Напоминание установлено на: {hour:02}:{minute:02}")
+        except ValueError:
+            mb.showerror("Ошибка", "Неверный формат времени")
 
 window = Tk()
 window.title("Напоминание")
